118efd02c55f969779c2904e2ca7e3606f8b70f6
BigGriz <waydbarton@hotmail.co.nz>
2020-11-03 18:00:33 +1300

    Initial Commit

---
+++ ARPG Movement/Assets/MovementController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementController : MonoBehaviour
+{
+    public GameObject bullet;
+
+    NavMeshAgent agent;
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100.0f))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100.0f))
+            {
+                Vector3 tempPoint = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+
+                // check loaded type - if direction
+                transform.LookAt(hit.point);
+                GameObject temp = Instantiate(bullet, transform.position + transform.forward, Quaternion.identity);
+                temp.transform.rotation = Quaternion.Euler(0.0f, transform.rotation.eulerAngles.y, 0.0f);
+            }
+        }
+    }
+}
---
+++ ARPG Movement/Assets/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damage;
+    public float speed;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer.ToString() == "Enemy")
+        {
+            Debug.Log(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
